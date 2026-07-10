65973f2e7abb393462c2593a9239bb8e1ab9ec5a
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2026-07-10 08:11:18 -0400

    ExtensionStats: Add Slicer 5.12.1
    See https://github.com/Slicer/Slicer/wiki/Release-Details#slicer-5121

--- ExtensionStats/ExtensionStats.py
+++ ExtensionStats/ExtensionStats.py
@@ -220,6 +220,7 @@ class ExtensionStatsLogic(ScriptedLoadableModuleLogic):
       '5.8.1': ['33241', '2025-03-02'],
       '5.10.0': ['34045', '2025-11-10'],
       '5.12.0': ['34621', '2026-06-24'],
+      '5.12.1': ['34624', '2026-07-09'],
       # NEXT RELEASE REVISION
     }
 
